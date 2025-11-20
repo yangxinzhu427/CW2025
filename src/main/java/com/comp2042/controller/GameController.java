1f0af2a946820ae2e3ff4e3016fe75753ab8b20c
yangxinzhu427 <xinzhuyang21@gmail.com>
2025-11-20 14:24:43 +0800

    fix: fixed a bug where the score didn't increase when blocks were dropped

--- src/main/java/com/comp2042/controller/GameController.java
+++ src/main/java/com/comp2042/controller/GameController.java
@@ -39,9 +39,9 @@ public class GameController implements InputEventListener {
             viewGuiController.refreshGameBackground(board.getBoardMatrix());
 
         } else {
-            if (event.getEventSource() == EventSource.USER) {
+            //if (event.getEventSource() == EventSource.USER) {
                 board.getScore().add(1);
-            }
+            //}
         }
         return new DownData(clearRow, board.getViewData());
     }
