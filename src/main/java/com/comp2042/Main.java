3a04c04cf8fba9736e25961010e65e22b888206e
yangxinzhu427 <xinzhuyang21@gmail.com>
2025-12-02 17:22:12 +0800

    fix: fixed a bug that fxml file failed to load

--- src/main/java/com/comp2042/Main.java
+++ src/main/java/com/comp2042/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        URL location = getClass().getClassLoader().getResource("mainMenuLayout.fxml");
+        URL location = getClass().getResource("/mainMenuLayout.fxml");
         ResourceBundle resources = null;
         FXMLLoader fxmlLoader = new FXMLLoader(location, resources);
         Parent root = fxmlLoader.load();
